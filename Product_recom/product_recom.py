44471c3db9ecdd978bc817593a2f4df29a67dcc9
Ashwin Kadam <53835307+ashwinkadam@users.noreply.github.com>
2023-04-27 13:56:25 -0400

    Update product_recom.py

--- Product_recom/product_recom.py
+++ Product_recom/product_recom.py
@@ -283,5 +283,5 @@ if check_user(user_id):
     st.subheader("Top buys from the past 🔁")
     st.write(top_buys_df)
 else:
-    st.subheader("Invalid User, User doesn't Exists ❌")
+    st.subheader("Invalid User, User does not Exists ❌")
 
